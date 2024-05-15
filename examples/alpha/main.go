3171741f20b0369aab109dfa0da829b444d15fad
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2024-05-15 15:43:26 -0500

    disable debug by default

--- examples/alpha/main.go
+++ examples/alpha/main.go
@@ -9,11 +9,6 @@ func main() {
 	itn.SetDebug(true)
 
 	processor := itn.NewLanguageES()
-	new_string := processor.Alpha2Digit(
-		"uno dos quince",
-		false,
-		true,
-		3,
-	)
+	new_string := processor.Alpha2Digit("uno dos quince", false, true, 3)
 	println(new_string)
 }
